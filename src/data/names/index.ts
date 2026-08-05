e34390594bd60fd9c61889bb3966bc138cc033a9
Cavzee <122266573+cavzee@users.noreply.github.com>
2026-08-05 19:55:13 +0100

    Build Pack 010: expand curated name library to 50 names

--- src/data/names/index.ts
+++ src/data/names/index.ts
@@ -1,11 +1,13 @@
 import a from "./a.json";
 import b from "./b.json";
 import c from "./c.json";
+import d from "./d.json";
 
 const names = [
   ...a,
   ...b,
   ...c,
+  ...d,
 ];
 
 export default names;
